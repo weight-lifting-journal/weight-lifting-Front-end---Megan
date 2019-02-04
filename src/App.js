0712c9ed2f2b2f627e36ffbbf6212f2ee28b5e14
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-04 17:42:53 -0600

    dummy home page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import Register from './components/Register';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path='/' component={Home}>Home</Route>
         <Route path='/users/login' component={Login}>Login</Route>
         <Route path='/users/register' component={Register}>Register</Route>
       </div>
