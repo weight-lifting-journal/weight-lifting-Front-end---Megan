2ba300727c74a483ffca7e84057823ed8cebefa7
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-07 11:04:56 -0600

    started on routToJournal by id

--- src/components/Workouts/WorkoutList.js
+++ src/components/Workouts/WorkoutList.js
@@ -5,14 +5,21 @@ import AddJournalForm from "./AddJournalForm";
 
 
 function WorkoutList(props) {
+    function routeToJournal(e, journal) {
+        e.preventDefault();
+        props.history.push(`/workouts/${journal.id}`);
+        props.getJournalById(journal.id)
+      }
 
     return (
         <div>
             {props.workouts.map(workout => (
-                <div key={workout.id}>
+                <div onClick={e => routeToJournal(e, journal)}
+                    key={workout.id}>
                     <h2>{workout.date}</h2>
                     <h3>{workout.region}</h3>
                     <button>Delete</button>
+                    <button>Add New Journal</button>
                 </div>
                 
             ))}
